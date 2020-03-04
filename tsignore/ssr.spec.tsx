5d2c42ce4d674570991a82e1055b1ee9f872ef1c
winton <mail@wintoni.us>
2020-03-03 22:21:25 -0800

    Ensure each test wrapped in describe

--- tsignore/ssr.spec.tsx
+++ tsignore/ssr.spec.tsx
@@ -9,44 +9,46 @@ import render from "../src/render"
 import ssr from "../src/ssr"
 import tinyId from "../src/tinyId"
 
-beforeEach(() => loaded.reset())
+describe("ssr", () => {
+  beforeEach(() => loaded.reset())
 
-it("renders", () => {
-  class MyComponent {
-    render: typeof render = null
+  it("renders", () => {
+    class MyComponent {
+      render: typeof render = null
 
-    id = "myComponent"
-    cache: Element
+      id = "myComponent"
+      cache: Element
 
-    element(): Element {
-      return this.cache || <div id={this.id} />
+      element(): Element {
+        return this.cache || <div id={this.id} />
+      }
     }
-  }
 
-  const myComponent = new MyComponent()
+    const myComponent = new MyComponent()
 
-  loaded.load({
-    dom: undom(),
-    logger,
-    myComponent,
-    patch,
-    render,
-    ssr,
-    tinyId,
+    loaded.load({
+      dom: undom(),
+      logger,
+      myComponent,
+      patch,
+      render,
+      ssr,
+      tinyId,
+    })
+
+    const ssrEl = <div id="myComponent" />
+    render.doc.body.appendChild(ssrEl)
+
+    const el = myComponent.element()
+    const getEl = render.doc.getElementById(el.id)
+
+    expect(el).toEqual(expect.any(Element))
+    expect(el.id).toBe("myComponent")
+
+    expect(el).toBe(getEl)
+    expect(el).not.toBe(ssrEl)
+
+    const el2 = myComponent.element()
+    expect(el).toBe(el2)
   })
-
-  const ssrEl = <div id="myComponent" />
-  render.doc.body.appendChild(ssrEl)
-
-  const el = myComponent.element()
-  const getEl = render.doc.getElementById(el.id)
-
-  expect(el).toEqual(expect.any(Element))
-  expect(el.id).toBe("myComponent")
-
-  expect(el).toBe(getEl)
-  expect(el).not.toBe(ssrEl)
-
-  const el2 = myComponent.element()
-  expect(el).toBe(el2)
 })
