988f631de3e62865aec354193f4dc34b5aff836b
winton <mail@wintoni.us>
2020-03-03 17:35:00 -0800

    Generator

---
+++ tsignore/ssr.spec.tsx
@@ -0,0 +1,52 @@
+import undom from "undom"
+
+import expect from "./expect"
+
+import patch from "../src/patch"
+import loaded from "../src/loaded"
+import logger from "../src/logger"
+import render from "../src/render"
+import ssr from "../src/ssr"
+import tinyId from "../src/tiny-id"
+
+beforeEach(() => loaded.reset())
+
+it("renders", () => {
+  class MyComponent {
+    render: typeof render = null
+
+    id = "myComponent"
+    cache: Element
+
+    element(): Element {
+      return this.cache || <div id={this.id} />
+    }
+  }
+
+  const myComponent = new MyComponent()
+
+  loaded.load({
+    dom: undom(),
+    logger,
+    myComponent,
+    patch,
+    render,
+    ssr,
+    tinyId,
+  })
+
+  const ssrEl = <div id="myComponent" />
+  render.doc.body.appendChild(ssrEl)
+
+  const el = myComponent.element()
+  const getEl = render.doc.getElementById(el.id)
+
+  expect(el).toEqual(expect.any(Element))
+  expect(el.id).toBe("myComponent")
+
+  expect(el).toBe(getEl)
+  expect(el).not.toBe(ssrEl)
+
+  const el2 = myComponent.element()
+  expect(el).toBe(el2)
+})
---
+++ tsignore/ssr.ts
@@ -0,0 +1,148 @@
+import { Fn2 } from "./loaded"
+
+export class Ssr {
+  dom: Window = null
+  fn2: Fn2 = null
+
+  loaded(): void {
+    for (const i in this.dom) {
+      if (i !== "document") {
+        global[i] = this.dom[i]
+      }
+    }
+
+    this.dom.document["getElementById"] = (
+      id: string
+    ): HTMLElement => {
+      return this.getElementById(this.dom.document.body, id)
+    }
+  }
+
+  async layout(
+    headComponent: any,
+    bodyComponent: any,
+    ...args: any[]
+  ): Promise<string> {
+    const elements: Record<string, Element> = {}
+    const runners = []
+
+    if (headComponent) {
+      runners.push({
+        head: (): Element => headComponent.element(...args),
+      })
+    }
+
+    if (bodyComponent) {
+      runners.push({
+        body: (): Element => bodyComponent.element(...args),
+      })
+    }
+
+    await this.fn2.run(elements, [], ...runners)
+
+    if (elements.body && elements.head) {
+      const body = this.serialize(elements.body)
+      const head = this.serialize(elements.head)
+
+      return `<!doctype html><html>${head}<body>${body}</body></html>`
+    } else if (elements.body) {
+      return this.serialize(elements.body)
+    }
+  }
+
+  script(
+    component: string,
+    libs: Record<string, string>,
+    components: Record<string, string>
+  ): string {
+    let imports = `  ${component}: import("${components[component]}"),\n`
+
+    for (const libName in libs) {
+      if (libName !== "loaded") {
+        imports += `  ${libName}: import("${libs[libName]}"),\n`
+      }
+    }
+
+    return `
+const stack = {
+${imports}}
+import("${libs.loaded}").then((lib) => {
+  window.loaded = lib.default
+  loaded.load(stack)
+  return loaded.wait("${component}", "renderClient")
+}).then(() => {
+  window.history.pushState({}, "", window.location.pathname)
+});`
+  }
+
+  serialize(el: Element): string {
+    if (el === undefined) {
+      return ""
+    }
+
+    if (el.nodeType === 3) {
+      return el.nodeValue
+    }
+
+    const name = String(el.nodeName).toLowerCase()
+    const hits: Record<string, boolean> = {}
+
+    let str = "<" + name
+    let c: string
+
+    for (let i = 0; i < el.attributes.length; i++) {
+      hits[el.attributes[i].name] = true
+      str +=
+        " " +
+        el.attributes[i].name +
+        '="' +
+        el.attributes[i].value +
+        '"'
+    }
+
+    if (el.className && !hits.class) {
+      str += ' class="' + el.className + '"'
+    }
+
+    if (el.id) {
+      str += ' id="' + el.id + '"'
+    }
+
+    if (el["value"]) {
+      str += ' value="' + el["value"] + '"'
+    }
+
+    str += ">"
+
+    for (let i = 0; i < el.childNodes.length; i++) {
+      c = this.serialize(el.childNodes[i] as Element)
+      if (c) {
+        str += c
+      }
+    }
+
+    return str + "</" + name + ">"
+  }
+
+  private getElementById(
+    el: HTMLElement,
+    id: string
+  ): HTMLElement {
+    if (el.id && el.id === id) {
+      return el
+    }
+
+    for (let i = 0; i < el.children.length; i++) {
+      const found = this.getElementById(
+        el.children[i] as HTMLElement,
+        id
+      )
+
+      if (found) {
+        return found
+      }
+    }
+  }
+}
+
+export default new Ssr()
